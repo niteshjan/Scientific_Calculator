39d9c97d9857d9f8ba7f7eaeab10b6e659395d39
Nitesh Jain <nitesh.jain@iiitb.org>
2021-03-11 20:53:55 +0530

    test cases added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,57 +1,93 @@
+
+import java.math.BigInteger;
 import java.util.*;
 import java.io.*;
+import com.google.common.math.BigIntegerMath;
+
 public class Calculator {
+    static double a,b,c;
+    static int choice;
+    static boolean check=true;
+    static Scanner sc=new Scanner(System.in);
+
     public static void main (String args[]) throws IOException{
-        double a,b,c;
-        int choice;
-        boolean check=true;
+
         while(check){
             try{
-                System.out.println("Press 1:Addition, 2:Subtraction, 3:Multiplication, 4:Division, 5:Exit");
+                System.out.println("Press 1:Square Root, 2:Factorial, 3:Natural Logarithm, 4:Power Funtion, 5:Exit");
                 System.out.print("Enter your choice:");
-                Scanner sc=new Scanner(System.in);
                 choice=sc.nextInt();
                 switch (choice){
-                    case 1: System.out.print("Enter two numbers with space:");
+                    case 1:
+                        System.out.print("Enter a positive number:");
                         a= sc.nextDouble();
-                        b= sc.nextDouble();
-                        c=a+b;
-                        System.out.println("Answer is: "+c);
+                        c=sqrt(a);
+                        if(c<0)
+                            System.out.println("Invalid number");
+                        else
+                            System.out.println("Answer is: "+c);
                         break;
-
-                    case 2: System.out.print("Enter two numbers with space:");
+                    case 3: System.out.print("Enter a number greater than 0:");
+                        double a= sc.nextDouble();
+                        c=log(a);
+                        if(c<0)
+                             System.out.println("Invalid number");
+                        else
+                             System.out.println("Answer is: "+ c);
+                        break;
+                    case 4:
+                        System.out.print("Enter two numbers with space (a^b: a b):");
                         a= sc.nextDouble();
                         b= sc.nextDouble();
-                        c=a-b;
+                        c=pow(a,b);
                         System.out.println("Answer is: "+ c);
                         break;
-                    case 3: System.out.print("Enter two numbers with space:");
-                        a= sc.nextDouble();
-                        b= sc.nextDouble();
-                        c=a*b;
-                        System.out.println("Answer is: "+ c);
+                    case 2:
+                        System.out.print("Enter a positive number:");
+                        int f=sc.nextInt();
+                        BigInteger fac= fact(f);
+                        int test= fac.compareTo(new BigInteger("0"));
+                        if(test<0)
+                            System.out.println("Invalid number");
+                        else
+                            System.out.println("Answer is: "+ fac);
                         break;
-                    case 4: System.out.print("Enter two numbers with space:");
-                        a= sc.nextDouble();
-                        b= sc.nextDouble();
-                        if(b==0) {
-                            System.out.println("Invalid division: Division with 0)");
-                            break;
-                        }
-                        c=a/b;
-                        System.out.println("Answer is: "+ c);
-                        break;
-                    case 5: System.out.println("Thank you for using Calculator");
+                    case 5:
+                        System.out.println("Thank you for using Calculator");
                         check=false;
                         break;
-                    default: System.out.println("Wrong Choice,enter a valid number");
-
+                    default:
+                        System.out.println("Wrong Choice,enter a valid number");
                 }
             }
             catch (Exception inp){
                 System.out.println("Error: "+inp );
             }
         }
-
+    }
+    public static double sqrt(double a){
+        if(a<0){
+            return -1;
+        }
+        c=Math.sqrt(a);
+        return c;
+    }
+    public static BigInteger fact(int f){
+        if(f<0){
+            return new BigInteger("-1");
+        }
+        BigInteger ans = BigIntegerMath.factorial(f);
+        return ans;
+    }
+    public static double log(double a){
+        if(a<=0){
+            return -1;
+        }
+        c=Math.log(a);
+        return c;
+    }
+    public static double pow(double a, double b){
+        c=Math.pow(a,b);
+        return c;
     }
 }
